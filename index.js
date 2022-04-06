002deae61c4b2aa38525a6e4245e7fad9ff74230
Andrej0001 <101658961+Andrej0001@users.noreply.github.com>
2022-04-06 17:26:53 +0200

    sad
    asd

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ console.log("ahoj");
 console.log("ahjkboj");
 console.log("ahjhjkoj");
 console.log("ahoj");
+console.log("ahoj");
+console.log("ahoj");
+onsole.log("ahoj");
+
+onsole.log("ahoj");
+
+
+onsole.log("ahoj");
