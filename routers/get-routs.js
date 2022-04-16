f7cf6e6eba89c25842c7f4c8f0caf4d1c29a445c
Andrej0001 <101658961+Andrej0001@users.noreply.github.com>
2022-04-16 15:26:58 +0200

    uprava

--- routers/get-routs.js
+++ routers/get-routs.js
@@ -7,13 +7,10 @@ const router = express.Router()
 router.get('/recipe/list', (req, res) => {
   
     const data =  db.Recipe.find().then((result)=> {
-       
-        
-        
        res.json(
              {result}
        )
-       
+
        
     })
 });
